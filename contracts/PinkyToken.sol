4f071e32fe9b6d8bcff42c29ea47ca9a93a8300d
toufique-imam <2013nuhash@gmail.com>
2023-12-22 16:31:53 +0600

    minter and airdropper role

--- contracts/PinkyToken.sol
+++ contracts/PinkyToken.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.20;
 
-//A basic erc20 token with minting and burning functionality
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PinkyToken is ERC20, Ownable {
-    constructor() ERC20("PinkyToken", "PINKY") Ownable(msg.sender) {
-        _mint(msg.sender, 1000000000 * 10 ** 18);
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PinkyToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant AIRDROPPER_ROLE = keccak256("AIRDROPPER_ROLE");
+    
+    constructor() ERC20("PinkyToken", "PINKY") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function airDropSingle(address _to, uint256 _amount) external onlyOwner {
+    function airDropSingle(address _to, uint256 _amount) external onlyRole(AIRDROPPER_ROLE) {
         // transfer _amount to _to
         transfer(_to, _amount);
     }
@@ -18,10 +21,13 @@ contract PinkyToken is ERC20, Ownable {
     function airDropBulk(
         address[] memory _to,
         uint256[] memory _amount
-    ) external onlyOwner {
+    ) external onlyRole(AIRDROPPER_ROLE) {
         require(_to.length == _amount.length, "Invalid input");
         for (uint256 i = 0; i < _to.length; i++) {
             transfer(_to[i], _amount[i]);
         }
+    }
+    function mint(address _to, uint256 _amount) external onlyRole(MINTER_ROLE) {
+        _mint(_to, _amount);
     }
 }
