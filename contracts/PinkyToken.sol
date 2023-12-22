ecb55904624965533872a57056373c0754ddea40
toufique-imam <2013nuhash@gmail.com>
2023-12-22 22:19:52 +0600

    update erc20 token

--- contracts/PinkyToken.sol
+++ contracts/PinkyToken.sol
@@ -1,21 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PinkyToken is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant AIRDROPPER_ROLE = keccak256("AIRDROPPER_ROLE");
-    
-    constructor() ERC20("PinkyToken", "PINKY") {
+    uint256 public MAX_SUPPLY;
+
+    constructor(uint256 _maxSupply) ERC20("PinkyToken", "PINKY") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        MAX_SUPPLY = _maxSupply;
     }
 
-    function airDropSingle(address _to, uint256 _amount) external onlyRole(AIRDROPPER_ROLE) {
+    function airDropSingle(
+        address _to,
+        uint256 _amount
+    ) external onlyRole(AIRDROPPER_ROLE) {
         // transfer _amount to _to
-        transfer(_to, _amount);
+        _mint(_to, _amount);
     }
 
     function airDropBulk(
@@ -24,10 +28,16 @@ contract PinkyToken is ERC20, AccessControl {
     ) external onlyRole(AIRDROPPER_ROLE) {
         require(_to.length == _amount.length, "Invalid input");
         for (uint256 i = 0; i < _to.length; i++) {
-            transfer(_to[i], _amount[i]);
+            _mint(_to[i], _amount[i]);
         }
     }
+
     function mint(address _to, uint256 _amount) external onlyRole(MINTER_ROLE) {
         _mint(_to, _amount);
+    }
+
+    function setMaxSupply(uint256 _maxSupply) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_maxSupply > totalSupply(), "Invalid input");
+        MAX_SUPPLY = _maxSupply;
     }
 }
