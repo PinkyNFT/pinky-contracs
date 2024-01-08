0e842b39bcefeaad0de0900f08ca0516841f619a
toufique-imam <2013nuhash@gmail.com>
2024-01-08 23:55:31 +0600

    logo

--- contracts/PinkyNFT.sol
+++ contracts/PinkyNFT.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// ██████╗ ██╗███╗   ██╗██╗  ██╗██╗   ██╗
+// ██╔══██╗██║████╗  ██║██║ ██╔╝╚██╗ ██╔╝
+// ██████╔╝██║██╔██╗ ██║█████╔╝  ╚████╔╝ 
+// ██╔═══╝ ██║██║╚██╗██║██╔═██╗   ╚██╔╝  
+// ██║     ██║██║ ╚████║██║  ██╗   ██║   
+// ╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝   
+
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
